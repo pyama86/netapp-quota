3868d63c246e41dfc76dc407eb1216fd01963e11
pyama86 <pyama@pepabo.com>
2017-11-25 13:45:52 +0900

    fix timestamp

--- cli.go
+++ cli.go
@@ -12,6 +12,14 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func init() {
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	logrus.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
+
+}
+
 // Exit codes are int values that represent an exit code for a particular error.
 const (
 	ExitCodeOK    int = 0
